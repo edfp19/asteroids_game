1e68193777c30f5416fa1851d83ad770a476d8d2
Ever <78941506+edfp19@users.noreply.github.com>
2025-03-14 18:53:17 +0000

    Added the circleshape and the player classes. I also added a new constant to the constants file

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #this is the initial commit from local accenture laptop
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -14,6 +18,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
         pygame.time.Clock().tick(60)
+        player.draw(screen)
         dt = pygame.time.Clock().tick()/1000 # Convert milliseconds to seconds
 
 if __name__ == "__main__":
