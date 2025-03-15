c3dc72403c29180e196680c20e26d841acbf857b
Ever <78941506+edfp19@users.noreply.github.com>
2025-03-14 22:11:10 -0300

    Update main.py
    Documentation inside the code

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-#this is the initial commit from local accenture laptop
+#this is the initial commit from local laptop
 import pygame
+
+#game constants
 from constants import *
+#player values and drawings
 from player import Player
+#asteroid drawings
 from asteroids import Asteroid
+#this creates the asteroid field
 from asteroidfield import * 
 
+#iniatilize main function
 def main():
     pygame.init()
     pygame.time.Clock()
     dt = 0
+# display the screen with the values found in constants file. 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# this is done so you have a 'starting point' 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+# initialize clock
     clock = pygame.time.Clock()
+"""
+This creates pygame groups. This basically groups several objects that share some characteristics. 
+For example, updatable takes Player, Asteroid, AsteroidFields 
+"""
     updatable = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -20,17 +33,27 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawables)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
+# initialize the little thingy in the middle of the screen
     player = Player(x, y)
+# infinite loop for the game
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        # use the updatable group and run the update method described in player, asteroid etc
         updatable.update(dt)
+        #loop through every drawable and draw their members on screen
         for drawable in drawables:
             drawable.draw(screen)
+        """
+        update dt. That's just a delta of time that we use as a parameter for the object to move and things to refresh
+        60 FPS divided by 1000 coverts it into miliseconds 
+        I don't really know the reason behind this, gotta get into it
+        """
         dt = clock.tick(60)/1000 # Convert milliseconds to seconds
+        #flips the screen. Also don't know why
         pygame.display.flip()
-
+#this is so it can *just* run from main. You can't import it elsewhere
 if __name__ == "__main__":
     main()
